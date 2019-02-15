2a645ce701835fc4b0e6b6b6cf2f5ef68a0eee6b
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-15 10:38:58 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,3 +21,12 @@ user_name = gets.chomp
 
 puts say_hello(user_name)
 =end
+
+def help
+
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
